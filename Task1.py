1b1660973879362973285482d5e66fc1702cb234
kateryna <ts-kateryna.riabova@rakuten.com>
2020-08-15 18:52:30 +0300

    Change

--- Task1.py
+++ Task1.py
@@ -1,23 +1,29 @@
 from random import random
 
-M = 10
-N = 15
-list1 = []
+
+def countsum(numericList):
+    theSum = 0
+    for i in numericList:
+        theSum = theSum + i
+    return theSum
+
+
+M = int(input("Enter number of columns: "))
+N = int(input("Enter number of rows: "))
+a = []
 for i in range(N):
     b = []
     for j in range(M):
         b.append(int(random() * 11))
-        print("%3d" % b[j], end='')
-    list1.append(b)
-    print('   |', sum(b))
+        print("%3d" % b[j], end='\t')
+    a.append(b)
+    print('   ', countsum(b))
 
-for i in range(M):
-    print(" --", end='')
 print()
 
 for i in range(M):
     s = 0
     for j in range(N):
-        s += list1[j][i]
-    print("%3d" % s, end='')
+        s += a[j][i]
+    print("%3d" % s, end='\t')
 print()
